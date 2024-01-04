14be0ba2622ccb634149d0575acd9d15ad520da6
FeMarzani <marzaniarrieche@gmail.com>
2024-01-04 19:57:46 -0300

    :sparkles: feat: adicionando classe pra realizar requisição na API

--- app/actions/actions.py
+++ app/actions/actions.py
@@ -7,21 +7,50 @@
 
 # This is a simple example for a custom action which utters "Hello World!"
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+import requests
+import json
+import os
+from typing import Any, Text, Dict, List
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+
+class ActionCallPokemonAPI(Action):
+    
+    def name(self) -> Text:
+        return "action_call_pokemon_api"
+
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        API_KEY = os.getenv('API_KEY')
+        headers = {"Authorization": f"Bearer {API_KEY}", "Content-type": "application/json"}
+        link = os.getenv('link')
+        id_modelo = os.getenv('id_modelo')
+
+        user_intent = tracker.latest_message['intent'].get('name')
+        user_intent = str(user_intent)
+
+        if user_intent == 'pokemon_api':
+
+            user_message = tracker.latest_message['text']
+            user_message = str(user_message)
+
+            try:
+                body_mensagem = {
+                    "model": id_modelo,
+                    "messages": [{"role": "user", "content": user_message}] 
+                }
+
+                body_mensagem = json.dumps(body_mensagem)
+
+                requisicao = requests.post(link, headers=headers, data=body_mensagem)
+                resposta = requisicao.json()
+                conteudo = resposta["choices"][0]["message"]["content"]
+
+                dispatcher.utter_message(f"Aqui estão algumas informações sobre Pokémon: {conteudo}")
+
+            except Exception as e:
+                dispatcher.utter_message("Desculpe, não consegui encontrar informações.")
+                
+        return []
+        
+
